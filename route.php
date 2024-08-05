d6b05f8b3762956678fb84cb76d95a944162e197
unknown <Esmatullah@Esmatullah-Hassani.Local.MIUAF>
2024-08-05 08:50:31 +0430

    first commit

---
+++ route.php
@@ -0,0 +1,1752 @@
+<?php
+
+route::get('st',function(){
+    $data = '
+    [
+ {
+  "number": 15540
+ },
+ {
+  "number": 15539
+ },
+ {
+  "number": 15538
+ },
+ {
+  "number": 15537
+ },
+ {
+  "number": 15536
+ },
+ {
+  "number": 15535
+ },
+ {
+  "number": 15534
+ },
+ {
+  "number": 15533
+ },
+ {
+  "number": 15532
+ },
+ {
+  "number": 15531
+ },
+ {
+  "number": 15530
+ },
+ {
+  "number": 15529
+ },
+ {
+  "number": 15528
+ },
+ {
+  "number": 15527
+ },
+ {
+  "number": 15526
+ },
+ {
+  "number": 15525
+ },
+ {
+  "number": 15524
+ },
+ {
+  "number": 15523
+ },
+ {
+  "number": 15522
+ },
+ {
+  "number": 15521
+ },
+ {
+  "number": 15520
+ },
+ {
+  "number": 15519
+ },
+ {
+  "number": 15518
+ },
+ {
+  "number": 15517
+ },
+ {
+  "number": 15516
+ },
+ {
+  "number": 15515
+ },
+ {
+  "number": 15514
+ },
+ {
+  "number": 15513
+ },
+ {
+  "number": 15512
+ },
+ {
+  "number": 15511
+ },
+ {
+  "number": 15510
+ },
+ {
+  "number": 15509
+ },
+ {
+  "number": 15508
+ },
+ {
+  "number": 15507
+ },
+ {
+  "number": 15506
+ },
+ {
+  "number": 15505
+ },
+ {
+  "number": 15504
+ },
+ {
+  "number": 15503
+ },
+ {
+  "number": 15502
+ },
+ {
+  "number": 15501
+ },
+ {
+  "number": 15500
+ },
+ {
+  "number": 15499
+ },
+ {
+  "number": 15498
+ },
+ {
+  "number": 15497
+ },
+ {
+  "number": 15496
+ },
+ {
+  "number": 15495
+ },
+ {
+  "number": 15494
+ },
+ {
+  "number": 15493
+ },
+ {
+  "number": 15492
+ },
+ {
+  "number": 15491
+ },
+ {
+  "number": 15490
+ },
+ {
+  "number": 15489
+ },
+ {
+  "number": 15488
+ },
+ {
+  "number": 15487
+ },
+ {
+  "number": 15486
+ },
+ {
+  "number": 15485
+ },
+ {
+  "number": 15484
+ },
+ {
+  "number": 15483
+ },
+ {
+  "number": 15482
+ },
+ {
+  "number": 15481
+ },
+ {
+  "number": 15480
+ },
+ {
+  "number": 15479
+ },
+ {
+  "number": 15478
+ },
+ {
+  "number": 15477
+ },
+ {
+  "number": 15476
+ },
+ {
+  "number": 15475
+ },
+ {
+  "number": 15474
+ },
+ {
+  "number": 15473
+ },
+ {
+  "number": 15472
+ },
+ {
+  "number": 15471
+ },
+ {
+  "number": 15470
+ },
+ {
+  "number": 15469
+ },
+ {
+  "number": 15468
+ },
+ {
+  "number": 15467
+ },
+ {
+  "number": 15466
+ },
+ {
+  "number": 15464
+ },
+ {
+  "number": 15463
+ },
+ {
+  "number": 15462
+ },
+ {
+  "number": 15461
+ },
+ {
+  "number": 15460
+ },
+ {
+  "number": 15459
+ },
+ {
+  "number": 15458
+ },
+ {
+  "number": 15457
+ },
+ {
+  "number": 15456
+ },
+ {
+  "number": 15455
+ },
+ {
+  "number": 15454
+ },
+ {
+  "number": 15453
+ },
+ {
+  "number": 15452
+ },
+ {
+  "number": 15451
+ },
+ {
+  "number": 15450
+ },
+ {
+  "number": 15449
+ },
+ {
+  "number": 15448
+ },
+ {
+  "number": 15447
+ },
+ {
+  "number": 15446
+ },
+ {
+  "number": 15445
+ },
+ {
+  "number": 15444
+ },
+ {
+  "number": 15443
+ },
+ {
+  "number": 15442
+ },
+ {
+  "number": 15441
+ },
+ {
+  "number": 15440
+ },
+ {
+  "number": 15439
+ },
+ {
+  "number": 15438
+ },
+ {
+  "number": 15437
+ },
+ {
+  "number": 15436
+ },
+ {
+  "number": 15435
+ },
+ {
+  "number": 15434
+ },
+ {
+  "number": 15433
+ },
+ {
+  "number": 15432
+ },
+ {
+  "number": 15431
+ },
+ {
+  "number": 15430
+ },
+ {
+  "number": 15429
+ },
+ {
+  "number": 15428
+ },
+ {
+  "number": 15427
+ },
+ {
+  "number": 15426
+ },
+ {
+  "number": 15424
+ },
+ {
+  "number": 15423
+ },
+ {
+  "number": 15422
+ },
+ {
+  "number": 15421
+ },
+ {
+  "number": 15420
+ },
+ {
+  "number": 15419
+ },
+ {
+  "number": 15418
+ },
+ {
+  "number": 15417
+ },
+ {
+  "number": 15416
+ },
+ {
+  "number": 15415
+ },
+ {
+  "number": 15414
+ },
+ {
+  "number": 15413
+ },
+ {
+  "number": 15412
+ },
+ {
+  "number": 15411
+ },
+ {
+  "number": 15410
+ },
+ {
+  "number": 15409
+ },
+ {
+  "number": 15408
+ },
+ {
+  "number": 15407
+ },
+ {
+  "number": 15406
+ },
+ {
+  "number": 15405
+ },
+ {
+  "number": 15404
+ },
+ {
+  "number": 15403
+ },
+ {
+  "number": 15402
+ },
+ {
+  "number": 15401
+ },
+ {
+  "number": 15400
+ },
+ {
+  "number": 15399
+ },
+ {
+  "number": 15398
+ },
+ {
+  "number": 15397
+ },
+ {
+  "number": 15396
+ },
+ {
+  "number": 15395
+ },
+ {
+  "number": 15394
+ },
+ {
+  "number": 15393
+ },
+ {
+  "number": 15392
+ },
+ {
+  "number": 15391
+ },
+ {
+  "number": 15390
+ },
+ {
+  "number": 15389
+ },
+ {
+  "number": 15388
+ },
+ {
+  "number": 15387
+ },
+ {
+  "number": 15386
+ },
+ {
+  "number": 15385
+ },
+ {
+  "number": 15384
+ },
+ {
+  "number": 15383
+ },
+ {
+  "number": 15382
+ },
+ {
+  "number": 15381
+ },
+ {
+  "number": 15380
+ },
+ {
+  "number": 15379
+ },
+ {
+  "number": 15378
+ },
+ {
+  "number": 15377
+ },
+ {
+  "number": 15376
+ },
+ {
+  "number": 15375
+ },
+ {
+  "number": 15374
+ },
+ {
+  "number": 15373
+ },
+ {
+  "number": 15372
+ },
+ {
+  "number": 15371
+ },
+ {
+  "number": 15370
+ },
+ {
+  "number": 15369
+ },
+ {
+  "number": 15368
+ },
+ {
+  "number": 15367
+ },
+ {
+  "number": 15366
+ },
+ {
+  "number": 15365
+ },
+ {
+  "number": 15364
+ },
+ {
+  "number": 15363
+ },
+ {
+  "number": 15362
+ },
+ {
+  "number": 15361
+ },
+ {
+  "number": 15360
+ },
+ {
+  "number": 15359
+ },
+ {
+  "number": 15358
+ },
+ {
+  "number": 15357
+ },
+ {
+  "number": 15356
+ },
+ {
+  "number": 15355
+ },
+ {
+  "number": 15354
+ },
+ {
+  "number": 15353
+ },
+ {
+  "number": 15352
+ },
+ {
+  "number": 15351
+ },
+ {
+  "number": 15350
+ },
+ {
+  "number": 15349
+ },
+ {
+  "number": 15348
+ },
+ {
+  "number": 15347
+ },
+ {
+  "number": 15346
+ },
+ {
+  "number": 15345
+ },
+ {
+  "number": 15344
+ },
+ {
+  "number": 15343
+ },
+ {
+  "number": 15342
+ },
+ {
+  "number": 15341
+ },
+ {
+  "number": 15340
+ },
+ {
+  "number": 15339
+ },
+ {
+  "number": 15338
+ },
+ {
+  "number": 15337
+ },
+ {
+  "number": 15336
+ },
+ {
+  "number": 15335
+ },
+ {
+  "number": 15334
+ },
+ {
+  "number": 15333
+ },
+ {
+  "number": 15332
+ },
+ {
+  "number": 15331
+ },
+ {
+  "number": 15330
+ },
+ {
+  "number": 15329
+ },
+ {
+  "number": 15328
+ },
+ {
+  "number": 15327
+ },
+ {
+  "number": 15326
+ },
+ {
+  "number": 15325
+ },
+ {
+  "number": 15324
+ },
+ {
+  "number": 15323
+ },
+ {
+  "number": 15322
+ },
+ {
+  "number": 15321
+ },
+ {
+  "number": 15320
+ },
+ {
+  "number": 15319
+ },
+ {
+  "number": 15318
+ },
+ {
+  "number": 15317
+ },
+ {
+  "number": 15316
+ },
+ {
+  "number": 15315
+ },
+ {
+  "number": 15314
+ },
+ {
+  "number": 15313
+ },
+ {
+  "number": 15312
+ },
+ {
+  "number": 15311
+ },
+ {
+  "number": 15310
+ },
+ {
+  "number": 15309
+ },
+ {
+  "number": 15308
+ },
+ {
+  "number": 15307
+ },
+ {
+  "number": 15306
+ },
+ {
+  "number": 15305
+ },
+ {
+  "number": 15304
+ },
+ {
+  "number": 15303
+ },
+ {
+  "number": 15302
+ },
+ {
+  "number": 15301
+ },
+ {
+  "number": 15300
+ },
+ {
+  "number": 15299
+ },
+ {
+  "number": 15298
+ },
+ {
+  "number": 15297
+ },
+ {
+  "number": 15296
+ },
+ {
+  "number": 15295
+ },
+ {
+  "number": 15294
+ },
+ {
+  "number": 15293
+ },
+ {
+  "number": 15292
+ },
+ {
+  "number": 15291
+ },
+ {
+  "number": 15290
+ },
+ {
+  "number": 15289
+ },
+ {
+  "number": 15288
+ },
+ {
+  "number": 15287
+ },
+ {
+  "number": 15286
+ },
+ {
+  "number": 15285
+ },
+ {
+  "number": 15284
+ },
+ {
+  "number": 15283
+ },
+ {
+  "number": 15282
+ },
+ {
+  "number": 15281
+ },
+ {
+  "number": 15280
+ },
+ {
+  "number": 15279
+ },
+ {
+  "number": 15278
+ },
+ {
+  "number": 15277
+ },
+ {
+  "number": 15276
+ },
+ {
+  "number": 15275
+ },
+ {
+  "number": 15274
+ },
+ {
+  "number": 15273
+ },
+ {
+  "number": 15272
+ },
+ {
+  "number": 15271
+ },
+ {
+  "number": 15270
+ },
+ {
+  "number": 15269
+ },
+ {
+  "number": 15268
+ },
+ {
+  "number": 15267
+ },
+ {
+  "number": 15266
+ },
+ {
+  "number": 15265
+ },
+ {
+  "number": 15264
+ },
+ {
+  "number": 15263
+ },
+ {
+  "number": 15262
+ },
+ {
+  "number": 15261
+ },
+ {
+  "number": 15260
+ },
+ {
+  "number": 15259
+ },
+ {
+  "number": 15258
+ },
+ {
+  "number": 15257
+ },
+ {
+  "number": 15256
+ },
+ {
+  "number": 15255
+ },
+ {
+  "number": 15254
+ },
+ {
+  "number": 15253
+ },
+ {
+  "number": 15252
+ },
+ {
+  "number": 15251
+ },
+ {
+  "number": 15250
+ },
+ {
+  "number": 15249
+ },
+ {
+  "number": 15248
+ },
+ {
+  "number": 15247
+ },
+ {
+  "number": 15246
+ },
+ {
+  "number": 15245
+ },
+ {
+  "number": 15244
+ },
+ {
+  "number": 15243
+ },
+ {
+  "number": 15242
+ },
+ {
+  "number": 15241
+ },
+ {
+  "number": 15240
+ },
+ {
+  "number": 15239
+ },
+ {
+  "number": 15238
+ },
+ {
+  "number": 15237
+ },
+ {
+  "number": 15236
+ },
+ {
+  "number": 15235
+ },
+ {
+  "number": 15234
+ },
+ {
+  "number": 15233
+ },
+ {
+  "number": 15230
+ },
+ {
+  "number": 15225
+ },
+ {
+  "number": 15224
+ },
+ {
+  "number": 15223
+ },
+ {
+  "number": 15222
+ },
+ {
+  "number": 15221
+ },
+ {
+  "number": 15220
+ },
+ {
+  "number": 15219
+ },
+ {
+  "number": 15218
+ },
+ {
+  "number": 15217
+ },
+ {
+  "number": 15215
+ },
+ {
+  "number": 15214
+ },
+ {
+  "number": 15213
+ },
+ {
+  "number": 15212
+ },
+ {
+  "number": 15211
+ },
+ {
+  "number": 15210
+ },
+ {
+  "number": 15209
+ },
+ {
+  "number": 15208
+ },
+ {
+  "number": 15207
+ },
+ {
+  "number": 15206
+ },
+ {
+  "number": 15205
+ },
+ {
+  "number": 15204
+ },
+ {
+  "number": 15203
+ },
+ {
+  "number": 15202
+ },
+ {
+  "number": 15201
+ },
+ {
+  "number": 15200
+ },
+ {
+  "number": 15199
+ },
+ {
+  "number": 15198
+ },
+ {
+  "number": 15196
+ },
+ {
+  "number": 15195
+ },
+ {
+  "number": 15194
+ },
+ {
+  "number": 15193
+ },
+ {
+  "number": 15192
+ },
+ {
+  "number": 15191
+ },
+ {
+  "number": 15190
+ },
+ {
+  "number": 15189
+ },
+ {
+  "number": 15188
+ },
+ {
+  "number": 15187
+ },
+ {
+  "number": 15186
+ },
+ {
+  "number": 15185
+ },
+ {
+  "number": 15184
+ },
+ {
+  "number": 15183
+ },
+ {
+  "number": 15182
+ },
+ {
+  "number": 15181
+ },
+ {
+  "number": 15180
+ },
+ {
+  "number": 15179
+ },
+ {
+  "number": 15178
+ },
+ {
+  "number": 15177
+ },
+ {
+  "number": 15176
+ },
+ {
+  "number": 15175
+ },
+ {
+  "number": 15174
+ },
+ {
+  "number": 15173
+ },
+ {
+  "number": 15172
+ },
+ {
+  "number": 15171
+ },
+ {
+  "number": 15170
+ },
+ {
+  "number": 15163
+ },
+ {
+  "number": 15149
+ },
+ {
+  "number": 15148
+ },
+ {
+  "number": 15142
+ },
+ {
+  "number": 15141
+ },
+ {
+  "number": 15132
+ },
+ {
+  "number": 15128
+ },
+ {
+  "number": 15127
+ },
+ {
+  "number": 15126
+ },
+ {
+  "number": 15125
+ },
+ {
+  "number": 15104
+ },
+ {
+  "number": 15103
+ },
+ {
+  "number": 15097
+ },
+ {
+  "number": 12896
+ },
+ {
+  "number": 12895
+ },
+ {
+  "number": 12894
+ },
+ {
+  "number": 12893
+ },
+ {
+  "number": 12892
+ },
+ {
+  "number": 12891
+ },
+ {
+  "number": 12890
+ },
+ {
+  "number": 12889
+ },
+ {
+  "number": 12888
+ },
+ {
+  "number": 12887
+ },
+ {
+  "number": 12886
+ },
+ {
+  "number": 12885
+ },
+ {
+  "number": 12884
+ },
+ {
+  "number": 12883
+ },
+ {
+  "number": 12882
+ },
+ {
+  "number": 12881
+ },
+ {
+  "number": 12880
+ },
+ {
+  "number": 12879
+ },
+ {
+  "number": 12878
+ },
+ {
+  "number": 12877
+ },
+ {
+  "number": 12876
+ },
+ {
+  "number": 12875
+ },
+ {
+  "number": 12874
+ },
+ {
+  "number": 12873
+ },
+ {
+  "number": 12872
+ },
+ {
+  "number": 12871
+ },
+ {
+  "number": 12870
+ },
+ {
+  "number": 12869
+ },
+ {
+  "number": 12868
+ },
+ {
+  "number": 12867
+ },
+ {
+  "number": 12866
+ },
+ {
+  "number": 12865
+ },
+ {
+  "number": 12864
+ },
+ {
+  "number": 12863
+ },
+ {
+  "number": 12862
+ },
+ {
+  "number": 12861
+ },
+ {
+  "number": 12860
+ },
+ {
+  "number": 12859
+ },
+ {
+  "number": 12858
+ },
+ {
+  "number": 12857
+ },
+ {
+  "number": 12808
+ },
+ {
+  "number": 12807
+ },
+ {
+  "number": 12806
+ },
+ {
+  "number": 12805
+ },
+ {
+  "number": 12804
+ },
+ {
+  "number": 12803
+ },
+ {
+  "number": 12802
+ },
+ {
+  "number": 12801
+ },
+ {
+  "number": 12800
+ },
+ {
+  "number": 12799
+ },
+ {
+  "number": 12798
+ },
+ {
+  "number": 12797
+ },
+ {
+  "number": 12796
+ },
+ {
+  "number": 12795
+ },
+ {
+  "number": 12794
+ },
+ {
+  "number": 12793
+ },
+ {
+  "number": 12792
+ },
+ {
+  "number": 12791
+ },
+ {
+  "number": 12790
+ },
+ {
+  "number": 12789
+ },
+ {
+  "number": 12788
+ },
+ {
+  "number": 12787
+ },
+ {
+  "number": 12786
+ },
+ {
+  "number": 12785
+ },
+ {
+  "number": 12784
+ },
+ {
+  "number": 12783
+ },
+ {
+  "number": 12782
+ },
+ {
+  "number": 12781
+ },
+ {
+  "number": 12780
+ },
+ {
+  "number": 12779
+ },
+ {
+  "number": 12778
+ },
+ {
+  "number": 12777
+ },
+ {
+  "number": 12776
+ },
+ {
+  "number": 12775
+ },
+ {
+  "number": 12774
+ },
+ {
+  "number": 12773
+ },
+ {
+  "number": 12772
+ },
+ {
+  "number": 12771
+ },
+ {
+  "number": 12770
+ },
+ {
+  "number": 12769
+ },
+ {
+  "number": 12768
+ },
+ {
+  "number": 12767
+ },
+ {
+  "number": 12766
+ },
+ {
+  "number": 12765
+ },
+ {
+  "number": 12764
+ },
+ {
+  "number": 12763
+ },
+ {
+  "number": 12762
+ },
+ {
+  "number": 12761
+ },
+ {
+  "number": 12760
+ },
+ {
+  "number": 12759
+ },
+ {
+  "number": 12758
+ },
+ {
+  "number": 12757
+ },
+ {
+  "number": 12756
+ },
+ {
+  "number": 12755
+ },
+ {
+  "number": 12754
+ },
+ {
+  "number": 12753
+ },
+ {
+  "number": 12752
+ },
+ {
+  "number": 12744
+ },
+ {
+  "number": 12743
+ },
+ {
+  "number": 12742
+ },
+ {
+  "number": 12741
+ },
+ {
+  "number": 12740
+ },
+ {
+  "number": 12739
+ },
+ {
+  "number": 12738
+ },
+ {
+  "number": 12737
+ },
+ {
+  "number": 12736
+ },
+ {
+  "number": 12735
+ },
+ {
+  "number": 12734
+ },
+ {
+  "number": 12733
+ },
+ {
+  "number": 12732
+ },
+ {
+  "number": 12731
+ },
+ {
+  "number": 12730
+ },
+ {
+  "number": 12729
+ },
+ {
+  "number": 12728
+ },
+ {
+  "number": 12727
+ },
+ {
+  "number": 12726
+ },
+ {
+  "number": 12725
+ },
+ {
+  "number": 12724
+ },
+ {
+  "number": 12723
+ },
+ {
+  "number": 12712
+ },
+ {
+  "number": 12711
+ },
+ {
+  "number": 12710
+ },
+ {
+  "number": 12709
+ },
+ {
+  "number": 12708
+ },
+ {
+  "number": 12707
+ },
+ {
+  "number": 12706
+ },
+ {
+  "number": 12705
+ },
+ {
+  "number": 12704
+ },
+ {
+  "number": 12703
+ },
+ {
+  "number": 12701
+ },
+ {
+  "number": 12700
+ },
+ {
+  "number": 12699
+ },
+ {
+  "number": 12698
+ },
+ {
+  "number": 12697
+ },
+ {
+  "number": 12696
+ },
+ {
+  "number": 12695
+ },
+ {
+  "number": 12694
+ },
+ {
+  "number": 12693
+ },
+ {
+  "number": 12692
+ },
+ {
+  "number": 12691
+ },
+ {
+  "number": 12690
+ },
+ {
+  "number": 12689
+ },
+ {
+  "number": 12688
+ },
+ {
+  "number": 12687
+ },
+ {
+  "number": 12686
+ },
+ {
+  "number": 12685
+ },
+ {
+  "number": 12684
+ },
+ {
+  "number": 12683
+ },
+ {
+  "number": 12682
+ },
+ {
+  "number": 12681
+ },
+ {
+  "number": 12680
+ },
+ {
+  "number": 12679
+ },
+ {
+  "number": 12678
+ },
+ {
+  "number": 12677
+ },
+ {
+  "number": 12676
+ },
+ {
+  "number": 12675
+ },
+ {
+  "number": 12674
+ },
+ {
+  "number": 12673
+ },
+ {
+  "number": 12672
+ },
+ {
+  "number": 12671
+ },
+ {
+  "number": 12670
+ },
+ {
+  "number": 12669
+ },
+ {
+  "number": 12668
+ },
+ {
+  "number": 12667
+ },
+ {
+  "number": 12666
+ },
+ {
+  "number": 12665
+ },
+ {
+  "number": 12664
+ },
+ {
+  "number": 12663
+ },
+ {
+  "number": 12662
+ },
+ {
+  "number": 12661
+ },
+ {
+  "number": 12660
+ },
+ {
+  "number": 12659
+ },
+ {
+  "number": 12658
+ },
+ {
+  "number": 12657
+ },
+ {
+  "number": 12656
+ },
+ {
+  "number": 12655
+ },
+ {
+  "number": 12642
+ },
+ {
+  "number": 12641
+ },
+ {
+  "number": 12640
+ },
+ {
+  "number": 12639
+ },
+ {
+  "number": 12638
+ },
+ {
+  "number": 12628
+ },
+ {
+  "number": 12627
+ },
+ {
+  "number": 12626
+ },
+ {
+  "number": 12625
+ },
+ {
+  "number": 12618
+ },
+ {
+  "number": 12617
+ },
+ {
+  "number": 12616
+ },
+ {
+  "number": 12615
+ },
+ {
+  "number": 12614
+ },
+ {
+  "number": 12611
+ },
+ {
+  "number": 12610
+ },
+ {
+  "number": 12609
+ },
+ {
+  "number": 12608
+ },
+ {
+  "number": 12551
+ },
+ {
+  "number": 12169
+ },
+ {
+  "number": 12120
+ },
+ {
+  "number": 12116
+ },
+ {
+  "number": 12086
+ },
+ {
+  "number": 12021
+ },
+ {
+  "number": 12018
+ },
+ {
+  "number": 11912
+ },
+ {
+  "number": 11911
+ },
+ {
+  "number": 11910
+ },
+ {
+  "number": 11760
+ },
+ {
+  "number": 15542
+ }
+]
+';
+$numbers = collect(json_decode($data))->pluck('number');
+dd(App\Models\Transaction::onlyTrashed()->whereIn('number',$numbers)->update([
+    'deleted_at' => null
+]));
+});
